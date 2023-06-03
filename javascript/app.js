9a7dc91b27d28bbfdec6b82244372bb41e4885b2
Yago Santana <ysantanaf@gmail.com>
2023-06-03 00:14:49 -0300

    parte 2-dia 1

--- javascript/app.js
+++
@@ -1,8 +0,0 @@
-// Dia um
-let btnOne = document.querySelector('#btn1');
-let articleOne = document.querySelector('#article-one');
-
-
-btnOne.addEventListener('click', function() {
-    articleOne.classList.toggle('hide');
-});
